d6256eeef5f1fe0a18c4c008604a9d55ce57725b
Fuszenecker Róbert <robert.fuszenecker@outlook.com>
2025-01-10 20:23:25 +0100

    GC stats.

--- C#/Program.cs
+++ C#/Program.cs
@@ -7,9 +7,18 @@ internal static class Benchmark
 {
     public static int Main(string[] args)
     {
-        return Parser.Default
+        int result = Parser.Default
             .ParseArguments<Arguments>(args)
             .MapResult(ProcessArguments, errs => -1);
+
+        Console.WriteLine($"GC Heap Size (MB): {GC.GetGCMemoryInfo().HeapSizeBytes / 1048576: #,##0.00}");
+        Console.WriteLine($"GC 0 Collection Count: {GC.CollectionCount(0)}");
+        Console.WriteLine($"GC 1 Collection Count: {GC.CollectionCount(1)}");
+        Console.WriteLine($"GC 2 Collection Count: {GC.CollectionCount(2)}");
+        Console.WriteLine($"GC Total Pause Duration: {GC.GetTotalPauseDuration()}");
+        Console.WriteLine($"GC Pause Time Percentage: {GC.GetGCMemoryInfo().PauseTimePercentage:p}");
+
+        return result;
     }
 
     private static int ProcessArguments(Arguments opts)
