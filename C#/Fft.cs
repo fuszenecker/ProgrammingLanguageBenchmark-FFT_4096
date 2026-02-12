14694f1c48cdc7da3442ce891f0b4b85e41569e1
Fuszenecker Róbert <robert.fuszenecker@outlook.com>
2026-02-12 22:55:15 +0100

    Fix: Add missing SIMD intrinsics using directives

--- C#/Fft.cs
+++ C#/Fft.cs
@@ -2,6 +2,8 @@ using System;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 
 namespace CSharpFftDemo;
 
